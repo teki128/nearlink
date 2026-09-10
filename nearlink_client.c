ffc5675689abce3fd70bcfcc5c91d63eaa4b6954
Byte <burnjuice128@gmail.com>
2026-09-10 16:30:46 +0800

    重构nearlink_client和nearlink_server，添加扫描和连接状态处理逻辑，更新服务参数

--- nearlink_client.c
+++ nearlink_client.c
@@ -9,12 +9,30 @@
 #include "nearlink_common.h"
 #include "sle_ssap_client.h"
 
+uint16_t conn_handle;
+
 uint8_t client_id;
+
+sle_addr_t server_addr;
+
+static void sle_start_scan()
+{
+    sle_seek_param_t para = {.own_addr_type = 0,
+                             .filter_duplicates = 1,
+                             .seek_filter_policy = SLE_SEEK_FILTER_ALLOW_ALL,
+                             .seek_phys = SLE_SEEK_PHY_1M,
+                             .seek_type[0] = SLE_SEEK_ACTIVE,
+                             .seek_interval[0] = 100,
+                             .seek_window[0] = 100};
+
+    sle_set_seek_param(&para);
+    sle_start_seek();
+}
 
 static void sle_enable_cb(errcode_t status)
 {
     // 注册client身份
-    sle_uuid_t app_uuid = {.uuid = SLE_APP_UUID, .len = 2};
+    sle_uuid_t app_uuid = {.uuid = {0x00, 0xA0}, .len = 2};
     ssapc_register_client(&app_uuid, &client_id);
 
     // 设置本地地址
@@ -23,9 +41,79 @@ static void sle_enable_cb(errcode_t status)
     sle_set_local_addr(&addr);
 
     // 设置连接参数
-    sle_default_connect_param_t para = {
+    sle_default_connect_param_t para = {.enable_filter_policy = 0,
+                                        .initiate_phys = 1,
+                                        .gt_negotiate = SLE_ANNOUNCE_ROLE_G_CAN_NEGO,
+                                        .scan_interval = 200,
+                                        .scan_window = 20,
+                                        .min_interval = SLE_CONN_INTERVAL,
+                                        .max_interval = SLE_CONN_INTERVAL,
+                                        .timeout = 500};
 
-    };
+    sle_default_connection_param_set(&para);
+
+    // 扫描server
+    sle_start_scan();
+}
+
+static void sle_seek_result_cb(sle_seek_result_info_t *result)
+{
+    if (result == NULL) {
+        osal_printk("seek result is null!");
+        return;
+    }
+
+    // 寻找特定地址的server
+    if (memcmp(result->addr.addr, SLE_SERVER_ADDR, SLE_ADDR_LEN) == 0) {
+        server_addr = result->addr;
+        sle_stop_seek();
+    }
+}
+
+static void sle_stop_seek_cb(errcode_t status)
+{
+    if (status == ERRCODE_SUCC) {
+        sle_connect_remote_device(&server_addr);
+    }
+}
+
+static void sle_connect_state_changed_cb(uint16_t conn_id,
+                                         const sle_addr_t *addr,
+                                         sle_acb_state_t conn_state,
+                                         sle_pair_state_t pair_state,
+                                         sle_disc_reason_t disc_reason)
+{
+    if (conn_state == SLE_ACB_STATE_CONNECTED) {
+        osal_printk("sle_connect_state_changed_cb: connected, conn_id=0x%02x\r\n", conn_id);
+        conn_handle = conn_id;
+        // 首次连接尝试配对
+        if (pair_state == SLE_PAIR_NONE) {
+            osal_printk("start pairing...\r\n");
+            sle_pair_remote_device(&server_addr);
+        }
+    } else if (conn_state == SLE_ACB_STATE_DISCONNECTED) {
+        osal_printk("sle_connect_state_changed_cb: disconnected, restart scan\r\n");
+        conn_handle = 0;
+        sle_remove_paired_remote_device(&server_addr);
+        sle_start_scan();
+    }
+}
+
+static void sle_pair_complete_cb(uint16_t conn_id, const sle_addr_t *addr, errcode_t status)
+{
+    unused(conn_id);
+    unused(addr);
+
+    if (status == ERRCODE_SUCC) {
+        // 设置MTU大小
+        ssap_exchange_info_t para = {0};
+        para.mtu_size = SLE_MTU_SIZE;
+        para.version = 1;
+        ssapc_exchange_info_req(client_id, conn_id, &para);
+    } else {
+        sle_remove_paired_remote_device(addr);
+    }
+    osal_printk("sle_pair_complete_cb: %d\r\n", status);
 }
 
 static int sle_client_task(void)
@@ -34,6 +122,13 @@ static int sle_client_task(void)
     sle_connection_callbacks_t conn_cbs = {0};
 
     seek_cbs.sle_enable_cb = sle_enable_cb;
+    seek_cbs.seek_result_cb = sle_seek_result_cb;
+    seek_cbs.seek_disable_cb = sle_stop_seek_cb;
+
+    conn_cbs.connect_state_changed_cb = sle_connect_state_changed_cb;
+
+    sle_announce_seek_register_callbacks(&seek_cbs);
+    sle_connection_register_callbacks(&conn_cbs);
 
     enable_sle();
     return 0;
--- nearlink_server.c
+++ nearlink_server.c
@@ -14,57 +14,41 @@ uint16_t prop_handle;
 uint16_t conn_handle;
 
 uint8_t server_id;
-uint16_t conn_id;
 
 static errcode_t sle_start_service(void)
 {
     // 注册server身份
-    sle_uuid_t app_uuid = {.uuid = SLE_APP_UUID, .len = 2};
+    sle_uuid_t app_uuid = {.uuid = {0x00, 0xA0}, .len = 2};
     ssaps_register_server(&app_uuid, &server_id);
 
     // 添加service
-    sle_uuid_t server_uuid = {.uuid = SLE_SERVICE_UUID, .len = 2};
+    sle_uuid_t server_uuid = {.uuid = {0x01, 0xA0}, .len = 2};
     ssaps_add_service_sync(server_id, &server_uuid, true, &service_handle);
 
     // 添加service内的property
+    uint8_t prop_value[] = "hello sle";
     ssaps_property_info_t prop = {.uuid = {.uuid = {0x02, 0xA0}, .len = 2},
                                   .permissions = SSAP_PERMISSION_READ | SSAP_PERMISSION_WRITE,
                                   .operate_indication =
                                       SSAP_OPERATE_INDICATION_BIT_READ | SSAP_OPERATE_INDICATION_BIT_NOTIFY,
-                                  .value_len = sizeof("hello sle") - 1,
-                                  .value = "hello sle"};
+                                  .value_len = sizeof(prop_value) - 1,
+                                  .value = prop_value};
     ssaps_add_property_sync(server_id, service_handle, &prop, &prop_handle);
 
     // 添加property的配置(descriptor)
-    ssaps_desc_info_t desc = {.uuid = {.uuid = {0x03, 0xA0}, .len = 2},
-                              .permissions = SSAP_PERMISSION_READ | SSAP_PERMISSION_WRITE,
+    uint8_t desc_value[] = {1, 0};
+    ssaps_desc_info_t desc = {.permissions = SSAP_PERMISSION_READ | SSAP_PERMISSION_WRITE,
                               .operate_indication =
                                   SSAP_OPERATE_INDICATION_BIT_READ | SSAP_OPERATE_INDICATION_BIT_WRITE,
                               .type = SSAP_DESCRIPTOR_USER_DESCRIPTION,
                               .value_len = 2,
-                              .value = {1, 0}};
+                              .value = desc_value};
     ssaps_add_descriptor_sync(server_id, service_handle, prop_handle, &desc);
 
     ssaps_start_service(server_id, service_handle);
 
     return ERRCODE_SUCC;
 }
-
-// static void sle_enable_cb(errcode_t status)
-// {
-//     osal_printk("sle_enable_cb: %d\r\n", status);
-
-//     sle_addr_t addr = {.type = SLE_ADDRESS_TYPE_PUBLIC, .addr = NULL};
-//     memcpy(addr.addr, SLE_SERVER_ADDR, SLE_ADDR_LEN);
-//     sle_setlocal_addr(&addr);
-
-//     if (status == ERRCODE_SUCC) {
-//         sle_start_service();
-//         if (sle_init_adv() == ERRCODE_SUCC) {
-//             sle_start_adv();
-//         }
-//     }
-// }
 
 static void sle_start_service_cb(uint8_t server_id, uint16_t handle, errcode_t status)
 {
@@ -121,12 +105,12 @@ static errcode_t sle_init_adv(void)
     param.announce_gt_role = SLE_ANNOUNCE_ROLE_T_CAN_NEGO;
     param.announce_level = SLE_ANNOUNCE_LEVEL_NORMAL;
     param.announce_channel_map = SLE_ADV_CHANNEL_MAP_DEFAULT;
-    param.announce_interval_min = 0xC8; // 以25ms间隔广播
-    param.announce_interval_max = 0xC8;
-    param.conn_interval_min = 0x64; // 连接后期望以12.5ms间隔同步
-    param.conn_interval_max = 0x64;
-    param.conn_max_latency = 0x1F3;         // 最大休眠连接间隔
-    param.conn_supervision_timeout = 0x1F4; // 连接超时时间
+    param.announce_interval_min = 200; // 以25ms间隔广播, time=N*0.125ms
+    param.announce_interval_max = 200;
+    param.conn_interval_min = SLE_CONN_INTERVAL; // 连接后期望以12.5ms间隔同步
+    param.conn_interval_max = SLE_CONN_INTERVAL;
+    param.conn_max_latency = 499;         // 最大休眠连接间隔
+    param.conn_supervision_timeout = 500; // 连接超时时间
     param.announce_tx_power = 18;
     param.own_addr.type = SLE_ADDRESS_TYPE_PUBLIC;
     memcpy(param.own_addr.addr, SLE_SERVER_ADDR, SLE_ADDR_LEN); // 设置本端server地址
